a485aa76b2840ac5b8358eff5db22663582d3bfa
Marissa <78126525+mars2699@users.noreply.github.com>
2021-04-15 12:30:04 -0400

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -2,37 +2,45 @@
 """
 Created on Wed Feb  3 22:05:42 2021
 
-@author: mars2699
+@author: Marissa Murphy
 """
 
 import random
 
-# Def function marks a new function call. Our password is one big string.
-def shuffle(string):    
-  characterList = list(string)  # Store all pieces into single variable.
-  random.shuffle(characterList)
-  return ''.join(characterList) # End of function call, joins all elements 
-# I learned while making this code to use ''. for unseparated list elements. 
-# We are printing a password rather than sentences.
 
+print ("Welcome to my password generator and manager!")
+userChoice = input("Press 1 to generate a password, press 2 to add a new account, press 3 to remove an account, press 4 to look up an existing account, or press 5 to exit: ")
+
+def newPassword():
 # Randint randomizes between given range only
-uppercaseLetter1 = chr(random.randint(65,90))   
-lowercaseLetter2 = chr(random.randint(97,122)) 
-lowercaseLetter3 = chr(random.randint(97,122)) 
-lowercaseLetter4 = chr(random.randint(97,122)) 
-lowercaseLetter5 = chr(random.randint(97,122))
-lowercaseLetter6 = chr(random.randint(97,122))
-lowercaseLetter7 = chr(random.randint(97,122))
-lowercaseLetter8 = chr(random.randint(97,122))
-lowercaseLetter9 = chr(random.randint(97,122))
-lowercaseLetter10 = chr(random.randint(97,122))
-number11 = chr(random.randint(48,57))
-specCharacter = chr(random.randint(33,38))
+    uppercaseLetter1 = chr(random.randint(65,90))   
+    lowercaseLetter2 = chr(random.randint(97,122)) 
+    lowercaseLetter3 = chr(random.randint(97,122)) 
+    lowercaseLetter4 = chr(random.randint(97,122)) 
+    lowercaseLetter5 = chr(random.randint(97,122))
+    lowercaseLetter6 = chr(random.randint(97,122))
+    lowercaseLetter7 = chr(random.randint(97,122))
+    lowercaseLetter8 = chr(random.randint(97,122))
+    lowercaseLetter9 = chr(random.randint(97,122))
+    lowercaseLetter10 = chr(random.randint(97,122))
+    number11 = chr(random.randint(48,57))
+    specCharacter = chr(random.randint(33,38))
 
-password = uppercaseLetter1 + lowercaseLetter2 + lowercaseLetter3 + \
+    password = uppercaseLetter1 + lowercaseLetter2 + lowercaseLetter3 + \
             lowercaseLetter4 + lowercaseLetter5 + lowercaseLetter6 + \
             lowercaseLetter7 + lowercaseLetter8 + lowercaseLetter9 + \
             lowercaseLetter10 + number11 + specCharacter
-password = shuffle(password)    # We want a unique password every time
-
+    password = shuffle(password)    
 print('Your new password is: ',password)
+    
+def addAccount():
+    account = input("Account Title: ")
+    username = input("Username: ")
+    password = input("Password: ")
+
+def delAccount():
+
+def search():
+
+def bye():
+
