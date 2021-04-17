e440c0394849bcb6331439804ea372f346ee3427
Marissa <78126525+mars2699@users.noreply.github.com>
2021-04-16 22:14:24 -0400

    Update password_generator_manager.py

--- password_generator_manager.py
+++ password_generator_manager.py
@@ -6,13 +6,13 @@ Created on Wed Feb  3 22:05:42 2021
 """
 
 import random
-
+# import keyring
 
 print ("Welcome to my password generator and manager!")
 userChoice = input("Press 1 to generate a password, press 2 to add a new account, press 3 to remove an account, press 4 to look up an existing account, or press 5 to exit: ")
 
+#1
 def newPassword():
-# Randint randomizes between given range only
     uppercaseLetter1 = chr(random.randint(65,90))   
     lowercaseLetter2 = chr(random.randint(97,122)) 
     lowercaseLetter3 = chr(random.randint(97,122)) 
@@ -26,21 +26,30 @@ def newPassword():
     number11 = chr(random.randint(48,57))
     specCharacter = chr(random.randint(33,38))
 
-    password = uppercaseLetter1 + lowercaseLetter2 + lowercaseLetter3 + \
-            lowercaseLetter4 + lowercaseLetter5 + lowercaseLetter6 + \
-            lowercaseLetter7 + lowercaseLetter8 + lowercaseLetter9 + \
-            lowercaseLetter10 + number11 + specCharacter
-    password = shuffle(password)    
-print('Your new password is: ',password)
-    
+    passwordLinked = uppercaseLetter1 + lowercaseLetter2 + lowercaseLetter3 + lowercaseLetter4 + lowercaseLetter5 + lowercaseLetter6 + lowercaseLetter7 + lowercaseLetter8 + lowercaseLetter9 + lowercaseLetter10 + number11 + specCharacter
+    finalPassword = random.shuffle(passwordLinked)    
+    print('Your new password is: ',finalPassword)
+
+#2    
 def addAccount():
     account = input("Account Title: ")
     username = input("Username: ")
     password = input("Password: ")
 
+#3
 def delAccount():
+    deleteChoice = input("What account do you want to delete?: ")
 
+#4
 def search():
+    wantedAccount = input("Which account are you looking for?: ")
+    #get_keyring()
 
+#5
 def bye():
+    print("Bye for now!")
+    exit()
+
+if userChoice == "1":
+    newPassword()
 
